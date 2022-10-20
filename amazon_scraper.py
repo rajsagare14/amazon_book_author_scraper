019cac030ccfce42ae9c85c8df2551faaf48f8a9
Rajwardhan Sagare <58337889+rajsagare14@users.noreply.github.com>
2022-10-20 13:02:30 +0530

    Update amazon_scraper.py
    Removed Comments

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -11,18 +11,4 @@ driver.get(url)
 soup = BeautifulSoup(driver.page_source,'lxml')
 section = soup.find('a',class_='a-size-base').text
 print(section)
-# driver.close()
 ## Comments Below
-# from msedge.selenium_tools import Edge, EdgeOptions
-## Startup the Web-Driver
-# options = EdgeOptions()
-# options.use_chromium = True
-# driver = Edge(options = options)
-# print(book_name)
-# url = 'https://www.amazon.in/s?k=the+psychology+of+money&i=stripbooks&s=review-rank'
-# for auth in section:
-# 	if(auth.text):
-# 		auth_name = auth.text
-# 	else:
-# 		auth_name = ''
-	# print(auth_name)
